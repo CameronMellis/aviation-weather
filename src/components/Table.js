b1027608af618df77b251aaca33eacc5eb4abb7c
CameronMellis <88047548+CaMel-315@users.noreply.github.com>
2022-02-18 01:00:35 -0330

    table updates

--- src/components/Table.js
+++ src/components/Table.js
@@ -7,16 +7,16 @@ import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
 
-function createData(name, time, winddirection, windspeed, visibility, precip, ceiling) {
-  return { name, time, winddirection, windspeed, visibility, precip, ceiling };
+function createData(name, time, winddirection, windspeed, visibility, precip, ceiling, temp, dewpoint, altimeter) {
+  return { name, time, winddirection, windspeed, visibility, precip, ceiling, temp, dewpoint, altimeter };
 }
 
 const rows = [
-  createData('CYYT', 2200, 220, 24, "P6", "FG", "OVC007"),
-  createData('CYQX', 2220, 210, 18, "5", "", "OVC018"),
-  createData('CYDF', 2220, 240, 16, "P6", "", "SKC"),
-  createData('CYJT', 2220, 220, 23, "3", "-RA", "OVC005"),
-  createData('CYYR', 2220, 200, 14, "P6", "", "SKC"),
+  createData('CYYT', 2200, 220, 24, "P6", "FG", "OVC007","5", "5", 2985),
+  createData('CYQX', 2220, 210, 18, "5", "", "OVC018", "4", "7", 2970),
+  createData('CYDF', 2220, 240, 16, "P6", "", "SKC", "6", "7", 2972),
+  createData('CYJT', 2220, 220, 23, "3", "-RA", "OVC005", "2", "2", 2970),
+  createData('CYYR', 2220, 200, 14, "P6", "", "SKC", "-5", "0", 2994),
 ];
 
 export default function DenseTable() {
@@ -32,6 +32,9 @@ export default function DenseTable() {
             <TableCell align="right">Visibility&nbsp;(SM)</TableCell>
             <TableCell align="right">Precip</TableCell>
             <TableCell align="right">Ceiling&nbsp;(AGL)</TableCell>
+            <TableCell align="right">Temp&nbsp;(C)</TableCell>
+            <TableCell align="right">Dewpoint</TableCell>
+            <TableCell align="right">Altimeter&nbsp;(inHg)</TableCell>
           </TableRow>
         </TableHead>
         <TableBody>
@@ -49,6 +52,9 @@ export default function DenseTable() {
               <TableCell align="right">{row.visibility}</TableCell>
               <TableCell align="right">{row.precip}</TableCell>
               <TableCell align="right">{row.ceiling}</TableCell>
+              <TableCell align="right">{row.temp}</TableCell>
+              <TableCell align="right">{row.dewpoint}</TableCell>
+              <TableCell align="right">{row.altimeter}</TableCell>
             </TableRow>
           ))}
         </TableBody>
