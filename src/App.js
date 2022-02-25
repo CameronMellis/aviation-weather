77e43e4bd80968364f11b991370154feaf2274d4
CameronMellis <88047548+CaMel-315@users.noreply.github.com>
2022-02-24 22:00:10 -0330

    tried dark mode, doesnt work

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import React, { useEffect } from 'react';
 import Header from './components/Header';
 import Table from './components/Table';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
 import './App.css';
+import Paper from '@mui/material/Paper';
 
 const getAVWX = async () => {
   const headers = {
@@ -16,15 +18,24 @@ const getAVWX = async () => {
 };
 
 function App() {
+  const theme = createTheme({
+    palette: {
+      type: 'dark',
+    },
+  });
   useEffect(() => {
     getAVWX();
   });
 
   return (
-    <div className='App'>
-      <Header />
-      <Table />
-    </div>
+    <ThemeProvider theme={theme}>
+      <Paper style={{ height: '100vh' }}>
+        <div className='App'>
+          <Header />
+          <Table />
+        </div>
+      </Paper>
+    </ThemeProvider>
   );
 }
 
