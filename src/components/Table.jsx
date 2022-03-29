4afbf64c3f78fbdffd94d0952666a1bd5dfdbd54
CameronMellis <88047548+CaMel-315@users.noreply.github.com>
2022-03-29 14:38:09 -0230

    working on switching location

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -15,55 +15,85 @@ const theme = createTheme({
   },
 });
 
-function SubHeader (props){
-  let color = "#616161"
-  if (props.flight_rules === "VFR"){
-    color = "green"
+function SubHeader(props) {
+  let color = '#616161';
+  if (props.flight_rules === 'VFR') {
+    color = 'green';
+  } else if (props.flight_rules === 'MVFR') {
+    color = '#8bc34a';
+  } else if (props.flight_rules === 'IFR') {
+    color = 'yellow';
+  } else if (props.flight_rules === 'LIFR') {
+    color = 'red';
   }
-  else if (props.flight_rules === "MVFR"){
-    color = "#8bc34a"
-  }
-  else if (props.flight_rules === "IFR"){
-    color = "yellow"
-  }
-  else if (props.flight_rules === "LIFR"){
-    color = "red"
-  }
-  return(
-    <TableCell sx={{ fontSize: 15, fontWeight: 'bold', backgroundColor: color }} align='center'> {props.flight_rules}</TableCell>
-  )
-} 
+  return (
+    <TableCell
+      sx={{ fontSize: 15, fontWeight: 'bold', backgroundColor: color }}
+      align='center'
+    >
+      {' '}
+      {props.flight_rules}
+    </TableCell>
+  );
+}
 
 export default function DenseTable(props) {
   console.log(props);
-return (
+  return (
     <ThemeProvider theme={theme}>
       <TableContainer
-        sx={{ width: '100%', maxWidth: 650, maxHeight: 1205, fontSize: '15px', backgroundColor: '#616161' }}
-        component={Paper} className='Table'>
+        sx={{
+          width: '100%',
+          maxWidth: 650,
+          maxHeight: 1205,
+          fontSize: '15px',
+          backgroundColor: '#616161',
+        }}
+        component={Paper}
+        className='Table'
+      >
         <Typography variant='body1' component='div' gutterBottom>
           <Table size='small' aria-label='a dense table'>
             <TableHead>
-              <TableRow sx={{ backgroundColor: '#616161'}}>
+              <TableRow sx={{ backgroundColor: '#616161' }}>
                 <TableCell
-                  sx={{ fontSize: 15, fontWeight: 'bold', backgroundColor: '#616161'  }}
-                  align='center'>
+                  sx={{
+                    fontSize: 15,
+                    fontWeight: 'bold',
+                    backgroundColor: '#616161',
+                  }}
+                  align='center'
+                >
                   Flight Rules
                 </TableCell>
                 <TableCell
-                  sx={{ fontSize: 15, fontWeight: 'bold'}}
-                  align='left'>
+                  sx={{ fontSize: 15, fontWeight: 'bold' }}
+                  align='left'
+                >
                   METAR/TAF
                 </TableCell>
               </TableRow>
             </TableHead>
             <TableBody>
               {props.data.map((row) => (
-                <TableRow key={row.raw} sx={{ '&:last-child td, &:last-child th': { border: 0 } }}>               
+                <TableRow
+                  key={row.raw}
+                  sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                >
                   <SubHeader flight_rules={row.flight_rules}></SubHeader>
-                  <TableCell sx={{ fontSize: 16, fontWeight: 'medium', backgroundColor: '#8e8e8e' }} align='left' >{row.raw.replace(/BECMG|FM[0-9]*/g, (oldValue) => {
-                    return "\n" + oldValue})}</TableCell>
-                    </TableRow>
+                  <TableCell
+                    sx={{
+                      fontSize: 16,
+                      fontWeight: 'medium',
+                      backgroundColor: '#8e8e8e',
+                    }}
+                    align='left'
+                  >
+                    {row.raw.replace(/BECMG|FM[0-9]*/g, (oldValue) => {
+                      return '\n' + oldValue;
+                    })}
+                  </TableCell>
+                </TableRow>
               ))}
             </TableBody>
           </Table>
