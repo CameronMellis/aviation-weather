344a3d4b27ab185595593b7f17e23730f28d6b71
CameronMellis <88047548+CaMel-315@users.noreply.github.com>
2022-05-04 23:27:21 -0230

    fully functional v2

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import { ThemeProvider, createTheme } from "@mui/material/styles";
 import { Outlet, useNavigate, useParams } from "react-router-dom";
 import Grid from "@mui/material/Grid";
