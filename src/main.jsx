1f087e98efbc6c5b28f4c0167df1cbb21dac1517
CameronMellis <88047548+CaMel-315@users.noreply.github.com>
2022-05-06 18:00:28 -0230

    deploy3

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import App from "./App";
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter basename="/aviation-weather/">
+    <BrowserRouter basename="/aviation-weather">
       <Routes>
         <Route path="/" element={<App />} />
         <Route path="/:location" element={<App />} />
