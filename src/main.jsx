07317a4b0b9f15f5d3c64b6c53199a92223b88a0
CameronMellis <88047548+CaMel-315@users.noreply.github.com>
2022-08-10 14:12:28 -0230

    sign in/up components created

--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,16 @@ import ReactDOM from "react-dom";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./index.css";
 import App from "./App";
+import SignIn from "./components/SignIn";
+import SignUp from "./components/SignUp";
 
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter basename="/aviation-weather">
       <Routes>
         <Route path="/" element={<App />} />
+        <Route path="/signin" element={<SignIn />} />
+        <Route path="/signup" element={<SignUp />} />
         <Route path="/:location" element={<App />} />
       </Routes>
     </BrowserRouter>
